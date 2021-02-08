dceea63a5f16110feb69d5f476272eca1154f628
jeffersoncuartas <jefferson.cuartas@gmail.com>
2021-02-08 15:45:48 -0500

    commit tree da3588dd7aa6181d6faa8d447de9fd35e9c61737

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+
+//write a function to case-insentively match stringS
+//this function takes an array of drivers and a string as arguments, and return the matchling list of drivers.
+//The function should be case insensitive 
+
+function findMatching(source, sought) {
+    return source.filter ( possibleMatch => 
+        possibleMatch.toLowerCase() === sought.toLowerCase()
+        )
+}
+
+function fuzzyMatch (source, testString) {
+    return source.filter( possibleMatch =>
+        possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0 )
+}
+
+function matchName (source, soughtName) {
+    return source.filter( possibleMatch =>
+        possibleMatch.name === soughtName )
+}
+
